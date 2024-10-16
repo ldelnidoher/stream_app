dc92afd473b6129b1351a212098dae9c49194dc1
ldelnidoher <lucia.delnido@ua.es>
2024-10-16 10:25:45 +0200

    Update app_bulla.py

--- app_bulla.py
+++ app_bulla.py
@@ -219,25 +219,8 @@ try:
     st.write(f'Last updated: {d} UTC+2')
 
 except:
-    st.write(':hourglass: Data is currently being updated. This process might take a few minutes :hourglass:')
-
-
-
-    # a dedicated single loader 
-    with hc.HyLoader('Now doing loading',hc.Loaders.pulse_bars,):
-        time.sleep(loader_delay)
-    
-    # for 3 loaders from the standard loader group
-    with hc.HyLoader('Now doing loading',hc.Loaders.standard_loaders,index=[3,0,5]):
-        time.sleep(loader_delay)
-    
-    # for 1 (index=5) from the standard loader group
-    with hc.HyLoader('Now doing loading',hc.Loaders.standard_loaders,index=5):
-        time.sleep(loader_delay)
-    
-    # for 4 replications of the same loader (index=2) from the standard loader group
-    with hc.HyLoader('Now doing loading',hc.Loaders.standard_loaders,index=[2,2,2,2]):
-        time.sleep(loader_delay)
+    #st.write(':hourglass: Data is currently being updated. This process might take a few minutes :hourglass:')
+    st.spinner(text="Data is currently being updated. This process might take a few minutes...")
     
      
 
