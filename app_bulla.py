29c88dcdbd8d934dab6ee0e6348ba6332b0f07d2
ldelnidoher <lucia.delnido@ua.es>
2024-10-16 10:14:18 +0200

    Update app_bulla.py

--- app_bulla.py
+++ app_bulla.py
@@ -7,6 +7,7 @@ from main_ba import *
 import matplotlib.pyplot as plt
 import altair as alt
 import plotly.graph_objects as go
+import hydralit_components as hc
 #arreglar en todos intervalo dt en pred_dut1
 
 add_selectbox = st.sidebar.radio('Choose data to show:',
@@ -222,6 +223,22 @@ except:
 
 
 
- 
+    # a dedicated single loader 
+    with hc.HyLoader('Now doing loading',hc.Loaders.pulse_bars,):
+        time.sleep(loader_delay)
+    
+    # for 3 loaders from the standard loader group
+    with hc.HyLoader('Now doing loading',hc.Loaders.standard_loaders,index=[3,0,5]):
+        time.sleep(loader_delay)
+    
+    # for 1 (index=5) from the standard loader group
+    with hc.HyLoader('Now doing loading',hc.Loaders.standard_loaders,index=5):
+        time.sleep(loader_delay)
+    
+    # for 4 replications of the same loader (index=2) from the standard loader group
+    with hc.HyLoader('Now doing loading',hc.Loaders.standard_loaders,index=[2,2,2,2]):
+        time.sleep(loader_delay)
+    
+     
 
 
