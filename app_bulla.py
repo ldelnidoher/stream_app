334fb583de060c294c3c684436d7d02f3eba66d2
del Nido Herranz <lddelnido@externomf.es>
2024-11-04 14:30:02 +0100

    act historial

--- app_bulla.py
+++ app_bulla.py
@@ -271,7 +271,7 @@ try:
             dx_file = f'dx_{suffix}.txt'
             dy_file = f'dy_{suffix}.txt'
             dut1_file = f'dut1_{suffix}.txt'
-            if os.path.exists(xpol_file):
+            if not os.path.exists(xpol_file):
                 h = 'Columns: Date (yy/mm/dd), Epoch [MJD], KRR(xp, AAM xmass) [as], SSA 4PC + KRR [as], SSA 6PC + KRR [as], SSA 4PC + GPR [as], SSA 6PC + GPR [as]'
                 np.savetxt(xpol_file, xp_pred.iloc[:,:-1], fmt = ['%s','%d','% f','% f','% 1.5f','% 1.5f','% 1.5f'],delimiter=' \t',header=h)
                 h = 'Columns: Date (yy/mm/dd), Epoch [MJD], KRR (yp, dy)[as], KRR (yp, AAM ymass)[as], SSA 2PC + KRR [as], SSA 4PC + KRR [as], SSA 6PC + KRR [as]'
