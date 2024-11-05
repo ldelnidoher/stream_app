c022bdc1cc42acfa6b280f39763a073f63942518
ldelnidoher <lucia.delnido@ua.es>
2024-11-05 14:36:14 +0100

    Update app_bulla.py

--- app_bulla.py
+++ app_bulla.py
@@ -265,9 +265,7 @@ try:
                  'dUT1':np.array([]),
                 }
             )
-        st.write(estado)
         estado2 = os.path.exists(csv_file)
-        st.write(str(estado2))
         if day_of_week in {2,5}:
             #reload function and update 
             xpol_file = f'xpol_{suffix}.txt'
