dbe97e4d6e026cc7892fdc16e94d8406e498a11e
ldelnidoher <lucia.delnido@ua.es>
2025-01-09 14:19:42 +0100

    Update app_bulla.py

--- app_bulla.py
+++ app_bulla.py
@@ -42,9 +42,10 @@ if add_selectbox == "Models":
     db_path = r'db.db'
      
     conn = sqlite3.connect(db_path)
-     
+
     sql_query = """SELECT name FROM sqlite_master  
         WHERE type='table';"""
+    database = pd.read_sql(sql_query, conn) 
     cur = conn.cursor()
     cur.execute(sql_query)
     row = cur.fetchall()
