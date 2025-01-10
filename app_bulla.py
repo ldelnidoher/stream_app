cfd057c38ce130f9e37268679e04436240e28cb5
ldelnidoher <lucia.delnido@ua.es>
2025-01-10 13:03:31 +0100

    Update app_bulla.py

--- app_bulla.py
+++ app_bulla.py
@@ -65,10 +65,11 @@ if add_selectbox == "Models":
     st.subheader(f'Files for {selected}')
      
     df2 = dff[dff['param']==val]
-    pdate = st.radio(label='Publication date:',options = df2.pub_date.values)
+    #pdate = st.radio(label='Publication date:',options = df2.pub_date.values)
      
     pred = df2['values'].drop_duplicates()
-    pred_choice = st.selectbox('select a date',pdate)
+    
+    pred_choice = st.selectbox('select a date',df2.pub_date.values)
     years = df2['pub_date'].loc[df2['values'] == pred_choice]
     years_choice = st.selectbox('', years)
     eam = df2['type_EAM'].loc[df2['values'] == pred_choice]
