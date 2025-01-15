ef8aede1ac05603b3409f7e0f6048ffa17f0ad04
ldelnidoher <lucia.delnido@ua.es>
2025-01-15 10:30:31 +0100

    Update app_bulla.py

--- app_bulla.py
+++ app_bulla.py
@@ -115,7 +115,7 @@ if add_selectbox == "EOP predictions":
          fm = '% .8f'
      
     df = pd.DataFrame({'Date':dates_fmt,'Epoch [MJD]':epochs, f'w/o EAM [{txt}]':conv1, f'w/ EAM [{txt}]':conv2}, index = (['Day'+str(v) for v in range(11)]))
-    s = df.style.set_table_styles([{"selector":"thead",
+    s = df.style.set_table_styles([{"selector":"th.col_heading,td",
                                     "props":"background-color:orange;"},])
     st.dataframe(s, use_container_width = True)
      
