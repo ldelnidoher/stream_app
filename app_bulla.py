d18ee125dbebf28884bbf4a6397a2df4f68d5d17
ldelnidoher <lucia.delnido@ua.es>
2025-01-15 11:00:50 +0100

    Update app_bulla.py

--- app_bulla.py
+++ app_bulla.py
@@ -116,7 +116,7 @@ if add_selectbox == "EOP predictions":
      
     df = pd.DataFrame({'Date':dates_fmt,'Epoch [MJD]':epochs, f'w/o EAM [{txt}]':conv1, f'w/ EAM [{txt}]':conv2}, index = (['Day'+str(v) for v in range(11)]))
     #styles = [dict(selector="th", props=[('background-color','#fb9a5a'),('color','white')])] 
-    styles = [dict(selector="", props=[('border','1px solid #fb9a5a')]), dict(selector="th", props=[("background-color","#b2d6fb")])] 
+    styles = [dict(selector="", props=[('border','2px solid #fb9a5a')]), dict(selector="th", props=[("background-color","#b2d6fb")])] 
     s = df.style.set_table_styles(styles)
     st.table(s)
      
