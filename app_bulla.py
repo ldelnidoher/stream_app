52624b8f77393f0a43ffd0bf412a06ef1d217c61
ldelnidoher <lucia.delnido@ua.es>
2025-02-20 13:01:45 +0100

    Update app_bulla.py

--- app_bulla.py
+++ app_bulla.py
@@ -190,7 +190,7 @@ if menu == "PREDICTION MODELS":
     st.subheader("Prediction models without EAM")
     st.write('-For **xpol** prediction, each component is preprocessed by applying **Singular Spectrum Analysis (SSA)** in order to obtain a reconstructed time series and the residual noise time series. Using the **KRR** algorithm, two models are trained: one to predict the reconstructed time series and the other to predict the noise. Both predictions are then added to generate the final xpol prediction. Idem **ypol**.')
     st.write('-For the **dX** prediction, the **xFCN** component is calculated, and alongside **dX**, a model is trained using **KRR** to predict dX. Idem **dY**.')
-    st.write('-For the **dUT1** prediction, the data is altered by removing the leap seconds. Afterwards, a model is trained using **KRR** to predict this modified dUT1 time series. Lastly, the leap seconds are added back to obtain the final **dUT1** prediction.')
+    st.write('-For the **dUT1** prediction, the data is preprocessed by removing the leap seconds. Afterwards, a model is trained using **KRR** to predict this modified dUT1 time series. Lastly, the leap seconds are added back to obtain the final **dUT1** prediction.')
     st.image('esquema_noeam.png',output_format = 'png', use_container_width = False)
 
      
