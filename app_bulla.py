518ab89bf2d456a6828169e76111535b4cf68b93
ldelnidoher <lucia.delnido@ua.es>
2025-06-25 13:11:47 +0200

    Update app_bulla.py

--- app_bulla.py
+++ app_bulla.py
@@ -251,7 +251,7 @@ if menu == "PREDICTION MODELS":
     st.markdown('- For the **dX** prediction, the **Free Core Nutation (FCN)** x component (xFCN) is calculated, and alongside dX they are used to train a model using **KRR** to predict dX. Idem **dY**.')
     st.markdown('- For the **dUT1** prediction, the data is preprocessed by removing the leap seconds. Afterwards, a model is trained using **KRR** to predict this modified dUT1 time series. Lastly, the leap seconds are added back to obtain the final dUT1 prediction.')
     #url = 'https://github.com/ldelnidoher/stream_app/blob/d96b9356e65ab39e2da50f92a9ddf37790d6f755/esquema.png'
-    st.image('esquema.png',output_format = 'PNG',width = 1400) 
+    st.image('esquema.png',output_format = 'PNG',width = 1420) 
     st.divider()
 
     st.subheader("Prediction models using EAM")
@@ -259,7 +259,7 @@ if menu == "PREDICTION MODELS":
     st.markdown("- For **xpol** prediction, each component is preprocessed by applying **Singular Spectrum Analysis (SSA)** in order to obtain a reconstructed time series and the residual noise time series. Using this parameters alongside xEAM a model is trained using **Kernel Ridge Regression (KRR)** algorithm to predict xpol. Idem **ypol**.")
     st.markdown("- For the **dX** prediction, the **Free Core Nutation (FCN)** x component (xFCN) is calculated, and alongside dX and xEAM they are used to train a model using **KRR** to predict dX. Idem **dY**.")
     st.markdown("- For the **dUT1** prediction, the data is preprocessed by removing the leap seconds. Afterwards, alongside with zEAM, a model is trained using **KRR** to predict this modified dUT1 time series. Lastly, the leap seconds are added back to obtain the final dUT1 prediction.")
-    st.image('esquema_eam.png',output_format = 'PNG',,width = 1400)
+    st.image('esquema_eam.png',output_format = 'PNG',,width = 1420)
      
 d = datetime.datetime.now().date()
 
