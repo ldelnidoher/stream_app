0db2ea09788662b3388f34d9ef0864e2f046a6f1
del Nido Herranz <lddelnido@externomf.es>
2025-06-25 10:23:47 +0200

    act fmt .txt

--- functions_app.py
+++ functions_app.py
@@ -163,14 +163,14 @@ def history(dff,dff2):
     a1,a2 = history2(dff2)
     
     dd = len(df_no_hist)-len(a1)
-    aux = np.concatenate((np.array(['nan    ']*dd),np.array([f'{x:% .5f}' for x in a1['dX_new[mas]'].values])))
-    aux2 = np.concatenate((np.array(['nan    ']*dd),np.array([f'{x:% .5f}' for x in a1['dY_new[mas]'].values])))
+    aux = np.concatenate((np.array([' nan  ']*dd),np.array([f'{x: .5f}' for x in a1['dX_new[mas]'].values])))
+    aux2 = np.concatenate((np.array([' nan  ']*dd),np.array([f'{x: .5f}' for x in a1['dY_new[mas]'].values])))
     df_no_hist.insert(8,'dX_new[mas]',aux)
     df_no_hist.insert(9,'dY_new[mas]',aux2)
     
     dd = len(df_si_hist)-len(a2)
-    aux = np.concatenate((np.array(['nan    ']*dd),np.array([f'{x:.5f}' for x in a2['dX_new[mas]'].values])))
-    aux2 = np.concatenate((np.array(['nan    ']*dd),np.array([f'{x:.5f}' for x in a2['dY_new[mas]'].values])))
+    aux = np.concatenate((np.array([' nan  ']*dd),np.array([f'{x: .5f}' for x in a2['dX_new[mas]'].values])))
+    aux2 = np.concatenate((np.array([' nan  ']*dd),np.array([f'{x: .5f}' for x in a2['dY_new[mas]'].values])))
     df_si_hist.insert(8,'dX_new[mas]',aux)
     df_si_hist.insert(9,'dY_new[mas]',aux2)
     
