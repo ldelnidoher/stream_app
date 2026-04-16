43b98edd2524a0a5e0c3b08c17a0fd261a8ea725
alesaz <becario.adsaz@externomf.es>
2026-04-16 11:47:23 +0200

    update

--- update_fcn.py
+++ update_fcn.py
@@ -6,20 +6,19 @@ Created on Fri Jan 30 09:31:18 2026
 """
 
 import numpy as np
-import math
 from astropy.time import Time
 import os
+import shutil
 import sqlite3
 from contextlib import closing
+import webbrowser
+from platformdirs import user_downloads_dir
+import time
+import zipfile
+import sys
 
-#Path to app local repository
+# Path to app local repository
 direc2 = "C:/Users/becario.adsaz/Documents/Prueba/stream_app"
-
-day = 61139
-
-# URL Santiago
-url = 'https://unialicante-my.sharepoint.com/:u:/g/personal/santiago_belda_mscloud_ua_es/IQDmLGAPUE3CSbJmOpiesTZ6AZ9r3QPzk0kZfidSxidSllY?e=rRhHk6&download=1/FCN.zip/'
-
 
 ################### AUXILIARY  FUNCTIONS ################################
 def read_db(num,lista):
@@ -27,40 +26,17 @@ def read_db(num,lista):
         with closing(conn.cursor()) as cursor:
             cursor.execute(f""" {to_str(lista,num)}  """)
         conn.commit()   
-    #conn = sqlite3.connect(f"{direc2}/eop_predictions.db", timeout = 5)
-    #cursor = conn.cursor()
-    #cursor.execute(f""" {to_str(lista,num)}  """)
-    #conn.commit()
-    #conn.close()
     return 0
 
 """
 SI ERROR AL EJECUTAR CON RESTART EL KERNEL SUELE FUNCIONAR
 """
 
-def greg_to_mjd(f):   
-    """
-    Parameters
-    ----------
-    f : datetime.datetime
-        Gregorian time date
-
-    Returns
-    -------
-    mjd : int
-        f at 00:00h in MJD
-    """
-    y,m,d=f.timetuple()[:3]
-    jd = 367*y-int((7*(y+int((m+9)/12.0)))/4.0)+int((275*m)/9.0)+d+1721013.5-0.5*math.copysign(1,100*y+m-190002.5)+0.5
-    mjd = int(jd-2400000.5)
-    return mjd
-
 def to_str(lst,num):
     aa = ['eop_old','eop_new','fcn_cpo']
     st = f'INSERT INTO {aa[num]} (date, epoch, ac, [as], x0, y0, dx, dy) VALUES '
     for x in lst:
         st = st+str(tuple(x))+','
-    #print(st[:-1]+';')
     return st[:-1]+';'
 
 def delete_range(conn, id_inicio, id_fin, num):
@@ -80,30 +56,63 @@ def get_last_epoch(conn, num, param):
             
 ###################### FCN ###########################
 
+# Download file .zip from repository
+webbrowser.open("https://unialicante-my.sharepoint.com/:u:/g/personal/santiago_belda_mscloud_ua_es/IQDmLGAPUE3CSbJmOpiesTZ6AZ9r3QPzk0kZfidSxidSllY?e=rRhHk6&download=1", new=2)
+time.sleep(10)
+
+# Get downloads folder
+downloads = user_downloads_dir()
+print(downloads+'\CPO_FCN.zip')
+
+# Extract folder from .zip
+with zipfile.ZipFile(downloads+'\CPO_FCN.zip', 'r') as z:
+    z.extractall(downloads+'\CPO_FCN')
+
+# Get last day of last prediction and delete all predicted data from last update
 with closing(sqlite3.connect(f"{direc2}/eop_predictions.db")) as conn:
     last_day=get_last_epoch(conn, 2, 'epoch')
-    delete_range(conn, last_day-564, last_day, 2)
+    delete_range(conn, last_day-564, last_day, 2) # Remove last predicted data
 
-direc = direc2+'/fcn_cpo/FCN_CPO_IERS_C0420_20260329.txt'
+# Today
+mjd = int(Time.now().mjd)
+fday = (Time(mjd, format = 'mjd').to_datetime()).strftime('%Y%m%d')
 
-fday = (Time(day, format = 'mjd').to_datetime()).strftime('%Y%m%d')
-print(fday)
-dir1 = url+f'FCN_CPO_IERS_C0420_{fday}.txt'
+# Get today's filename
+dir1 = downloads+f'\CPO_FCN\FCN_CPO_IERS_C0420_{fday}.txt'
 print(dir1)
 
+# Open file
+try :
+    f= open(dir1,'r')
+    data = f.readlines()
+    f.close()
+except FileNotFoundError:
+    # If files are not updated, delete .zip and unpacked folder
+    print("No se ha encontrado el fichero actual")
+    file = os.path.join(downloads, "CPO_FCN.zip")
+    directory = os.path.join(downloads, "CPO_FCN")
+    if os.path.exists(file):
+        os.remove(file)
+        print("Archivo eliminado")
+    else:
+        print("El archivo no existe")
+    if os.path.exists(directory):
+        shutil.rmtree(directory)
+        print("Archivo eliminado")
+    else:
+        print("El archivo no existe")
+    sys.exit()
 
-f= open(direc,'r')
-data = f.readlines()
-f.close()
-
+# Get last day of new predictions
 new_day=int(float(data[-1].split()[0]))
 
+# Copy only needed data for the update
 data = list(np.transpose([j.split() for j in data[(last_day-565-new_day):]]))
 
+# Adapt data to database format
 data[0] = [int(float(x)) for x in data[0]]
 for j in range(1,7):
     data[j] = [float(x) for x in data[j]]
-    
 rows = []
 a= data[0]
 mjd = [Time(j,format = 'mjd') for j in a]
@@ -111,62 +120,22 @@ date = [j.iso[:-4] for j in mjd]
 rows.append(date)
 for j in range(0,7):
     rows.append(data[j])
-
 rows1 = [[fila[i] for fila in rows] for i in range(len(rows[0]))]
 
+# Update database with new data
 df_old= read_db(2,rows1)
 
-"""
-for i in range(2):
-    f = open(dirs1[i],'r')
-    data = f.readlines()
-    f.close()
-    
-    data = list(np.transpose([j.split() for j in data]))
-    
-    names = ['MJD','ac','as','x0','y0','dx','dy']
-    num = [0,1,2,3,7,8]
-    
-    data[0] = [int(x) for x in data[0]]
-    for j in range(1,6):
-        data[num[j]] = [float(x) for x in data[num[j]]]
-      
-    
-    rows1 = []
-    for j in range(len(names)):
-        aux = [d,names[j],i]
-        if num[j] == 0:
-            aux = aux+data[num[j]]
-        else:
-            aux = aux+ data[num[j]]
-        rows1.append(aux)
-     
-    df_old= read_db(0,rows1)
-    
-    
+# Delete .zip and unpacked folder
+file = os.path.join(downloads, "CPO_FCN.zip")
+directory = os.path.join(downloads, "CPO_FCN")
+if os.path.exists(file):
+    os.remove(file)
+    print("Archivo eliminado")
+else:
+    print("El archivo no existe")
+if os.path.exists(directory):
+    shutil.rmtree(directory)
+    print("Archivo eliminado")
+else:
+    print("El archivo no existe")
 
-    f = open(dirs2[i],'r')
-    data = f.readlines()
-    f.close()
-
-    data = list(np.transpose([j.split() for j in data]))
-
-    names = ['epoch','dx','dy']
-    num = [0,7,8]
-
-    data[0] = [int(x) for x in data[0]]
-    for j in range(1,3):
-        data[num[j]] = [float(x) for x in data[num[j]]]
-        
-    rows2 = []
-    for j in range(len(names)):
-        aux = [d,names[j],i]
-        if num[j] == 0:
-            aux = aux+data[num[j]]
-        else:
-            aux = aux+ data[num[j]]
-        rows2.append(aux)
-     
-    df_new = read_db(1,rows2)
-    
-"""
